33de90101246815318f998443f51b3529a4ce190
Chris <chris.haack@me.com>
2014-02-03 19:13:50 -0500

    add .trim method

--- public/memory.js
+++ public/memory.js
@@ -1,25 +1,27 @@
 $(document).ready(function () {
-  var $cards = $(".card.col6");
-  var count = 0; //set counter
+var $cards = $(".card.col6");
+var count = 0; //set counter
+var user_picks = []; //set match array
   $cards.on("click", function () { //2
     count++;
     var $this = $(this);
-    var match = [];
     if (count <= 2) {
-      $this.addClass("value").addClass("up");
+      $this.addClass("value").addClass("up"); // add classes
       $this.find(".value").fadeIn(); //show number
-      match.push($this.innerText);
-      console.log(match.length);
-      if (match.length === 2) {
-        if (match[0] === match[1]) {
-          console.log('confetti');
+      user_picks.push($this.find(".value").text().trim()); //push value to array
+      console.log(user_picks.length);
+      if (user_picks.length === 2 && user_picks[0] === user_picks[1]) {
+        console.log('confetti');
+        if ($cards.hasClass("up")) {
+          $this.addClass("disabled");
+        //user_picks.length = 0;
         }
-        else match.length = 0;
-      }
+      } 
     }
   });
 });
 
+// if($(this).find(".question").is(":visible"))
 
         // $this.find(".value").fadeOut();    //hide value
     //     // 3 change others state to disabled
